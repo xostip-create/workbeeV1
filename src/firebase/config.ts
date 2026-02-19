54247253caf96f47474c1b04a558ff0e87c2ff92
Emmanuel Solomon <xostip@gmail.com>
2026-02-19 18:36:30 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3513978291-72be2",
-  "appId": "1:91424518115:web:72c05dcb53635e25013ad6",
-  "apiKey": "AIzaSyCf1av9fQyZ2NPOLWlKtkbmSsx918yxzgg",
-  "authDomain": "studio-3513978291-72be2.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "91424518115"
+  apiKey: "AIzaSyCf1av9fQyZ2NPOLWlKtkbmSsx918yxzgg",
+  authDomain: "studio-3513978291-72be2.firebaseapp.com",
+  projectId: "studio-3513978291-72be2",
+  storageBucket: "studio-3513978291-72be2.firebasestorage.app",
+  messagingSenderId: "91424518115",
+  appId: "1:91424518115:web:72c05dcb53635e25013ad6"
 };
